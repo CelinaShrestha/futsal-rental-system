a8648a25f78408e91d0089635f9cd7bad533e6a6
Celina Shrestha <celinasth7@gmail.com>
2024-04-17 15:54:59 +0545

    added routes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,23 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Customer\CustomerController;
+
+use App\Http\Controllers\CustomerProfileController;
 use App\Http\Controllers\FutsalListingsController;
 use App\Http\Controllers\AdminProfileController;
 use App\Http\Controllers\VendorProfileController;
+
 use App\Http\Controllers\Admin\ProfileViewController;
+use App\Http\Controllers\Admin\ProfileEditController;
+use App\Http\Controllers\Admin\CustomerProfileEditController;
+use App\Http\Controllers\Admin\CourtEditController;
+
 use App\Http\Controllers\VendorAuth\RegisteredUserController;
+use App\Http\Controllers\Vendor\CourtViewController;
+
+use App\Http\Controllers\Court\BookingController;
+use App\Http\Controllers\Court\FilterController;
+
 use Inertia\Inertia;
 
 /*
@@ -31,11 +42,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Customer/Dashboard/index');
-})
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Customer/Dashboard/index');
+
+// })
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
 // Route::get('/Futsal', function () {
 //     return Inertia::render('Customer/FutsalListings/index');
@@ -45,14 +57,13 @@ Route::get('/dashboard', function () {
 // Route::delete('/futsal-listings/{id}', [FutsalListingsController::class, 'destroy'])->name('futsal-listings.destroy');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [CustomerProfileController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/booking', function () {
-        return Inertia::render('Customer/Booking/index');
-    })->name('book');
+    Route::get('/booking/{id}', [BookingController::class, 'show'])->name('book');
     Route::get('/futsal-listings', [FutsalListingsController::class, 'index'])->name('futsal-listings.index');
-
+    Route::post('/futsal-listings', [FilterController::class, 'filter'])->name('futsal-listings.filter');
     Route::get('/futsal-listings/{id}', [FutsalListingsController::class, 'show'])->name('futsal-listings.show');
 });
 
@@ -76,6 +87,18 @@ Route::middleware('auth:admin')->group(function () {
     Route::get('admin/courts', [ProfileViewController::class, 'futsalshow'])->name('admin.courts.show');
     Route::get('admin/Unverified_Courts', [ProfileViewController::class, 'unverified'])->name('admin.courts.unverified');
     Route::patch('admin/Unverified_Courts/{id}', [ProfileViewController::class, 'futsalverify'])->name('admin.court.verify');
+    Route::get('admin/court/profile/{id}', [CourtEditController::class, 'edit'])->name('admin.court.profile.edit');
+    Route::patch('admin/court/profile/{id}', [FutsalListingsController::class, 'update'])->name('admin.court.profile.update');
+    Route::delete('/court/profile/{id}', [FutsalListingsController::class, 'destroy'])->name('admin.court.profile.destroy');
+
+    Route::get('admin/customers', [ProfileViewController::class, 'customershow'])->name('customers.show');
+    Route::get('admin/customer/profile/{id}', [CustomerProfileEditController::class, 'edit'])->name('admin.customer.profile.edit');
+    Route::patch('admin/customer/profile/{id}', [CustomerProfileEditController::class, 'update'])->name('admin.customer.profile.update');
+    Route::delete('/customer/profile/{id}', [CustomerProfileEditController::class, 'destroy'])->name('admin.customer.profile.destroy');
+
+    Route::get('admin/vendor/profile/{id}', [ProfileEditController::class, 'edit'])->name('admin.vendor.profile.edit');
+    Route::patch('admin/vendor/profile/{id}', [ProfileEditController::class, 'update'])->name('admin.vendor.profile.update');
+    Route::delete('/vendor/profile/{id}', [ProfileEditController::class, 'destroy'])->name('admin.vendor.profile.destroy');
 });
 
 require __DIR__ . '/adminauth.php';
@@ -92,6 +115,8 @@ Route::middleware('auth:vendor')->group(function () {
     Route::delete('/vendor/profile', [VendorProfileController::class, 'destroy'])->name('vendor.profile.destroy');
     Route::get('/vendor/addCourt', [FutsalListingsController::class, 'create'])->name('futsal-listings.create');
     Route::post('/vendor/addCourt', [FutsalListingsController::class, 'store'])->name('futsal-listings.store');
+    // Route::post('/vendor/addTimeSlots', [FutsalListingsController::class, 'storeTimeSlots'])->name('time-slots.store');
+    Route::get('/vendor/courts', [CourtViewController::class, 'index'])->name('vendor.futsal-listings');
 });
 
 require __DIR__ . '/vendorauth.php';
