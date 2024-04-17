76e5f9bd35b6993166f9902aa8dcb08283f5b2e0
Celina Shrestha <celinasth7@gmail.com>
2024-04-17 15:55:56 +0545

    Work on Vendor Panel Controller

---
+++ app/Http/Controllers/Vendor/CourtViewController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\FutsalListings;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+
+class CourtViewController extends Controller
+{
+    public function index()
+    {
+        // Retrieve the authenticated user (Vendor instance)
+        $vendor = Auth::guard('vendor')->user();
+
+        // Retrieve futsal listings for the vendor
+        $futsal_listings = FutsalListings::where('vendor_id', $vendor->id)->get();
+
+        return Inertia::render('Vendor/Court/ViewCourt/index', [
+            'futsal_listings' => $futsal_listings,
+        ]);
+    }
+}
