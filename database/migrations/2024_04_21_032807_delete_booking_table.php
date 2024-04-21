12a42cedd0a30c5189fbd208ddb99a3dcca6a34b
Celina Shrestha <celinasth7@gmail.com>
2024-04-21 13:38:54 +0545

    Work on Database

--- database/migrations/2024_04_21_032807_delete_booking_table.php
+++ database/migrations/2024_04_21_032807_delete_booking_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::dropIfExists('time_slots');
+        Schema::table('bookings', function (Blueprint $table) {
+            Schema::dropIfExists('bookings');
+        });
     }
 
     /**
@@ -19,6 +20,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+        });
     }
 };
