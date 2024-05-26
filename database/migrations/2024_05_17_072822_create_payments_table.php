af8e8ee09845228422b85d9f1a300ae47f92f642
Celina Shrestha <celinasth7@gmail.com>
2024-05-26 21:59:05 +0545

    Work on migrations

--- database/migrations/2024_05_17_072822_create_payments_table.php
+++ database/migrations/2024_05_17_072822_create_payments_table.php
@@ -3,9 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Booking;
+use App\Models\User;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +14,18 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table
+                ->foreignIdFor(Booking::class)
+                ->constrained()
+                ->onDelete('cascade');
+            $table
+                ->foreignIdFor(User::class)
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('transaction_id')->unique();
+            $table->decimal('amount', 10, 2);
+            $table->boolean('is_cancelled')->default(false);
+            $table->boolean('is_refunded')->default(false);
             $table->timestamps();
         });
     }
